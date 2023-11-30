1ff10bea257bd9aa2e6ae2416bf2e1442ca50f0a
PauloVictorTSilva <pilutechinformatica@gmail.com>
2023-11-30 19:43:04 -0300

    Feat: add routing to social links

--- next.config.js
+++ next.config.js
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/pilu-vitu/",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/PiluVitu",
+        permanent: true,
+      },
+      {
+        source: "/instagram",
+        destination: "https://www.instagram.com/pilu.dev/",
+        permanent: true,
+      },
+      {
+        source: "/twitter",
+        destination: "https://twitter.com/PiluVitu",
+        permanent: true,
+      },
+      {
+        source: "/whatsapp",
+        destination: "https://wa.me/message/6UOZCKTOYGUNE1",
+        permanent: true,
+      },
+      {
+        source: "/twitch",
+        destination: "https://www.twitch.tv/piluvitu",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
