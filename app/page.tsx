53aed2bea925aa8f1e2ff34eeef013562443ef97
Paulo Victor <pilutechinformatica@gmail.com>
2024-04-26 12:35:42 -0300

    fix: ajust responsive (#6)

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { Project, Projects } from '@/mocks/projects'
 import { Social, Socials } from '@/mocks/social'
 export default function Home() {
   return (
-    <main className="min-h-screen items-start gap-24 p-20 2xl:grid 2xl:grid-cols-3">
+    <main className="min-h-screen items-start gap-24 p-2 lg:p-20 2xl:grid 2xl:grid-cols-3">
       <div
         id="left side"
         className="col-span-1 flex flex-col items-start justify-between"
