faa89d6ec4f5b0f6eadd1c6b91256a6dea7fbfe5
Paulo Victor T Silva <pilutechinformatica@gmail.com>
2024-04-30 13:50:03 -0300

    fix: cache time

--- components/article-card.tsx
+++ components/article-card.tsx
@@ -14,7 +14,7 @@ export function ArticleCard() {
       const response = await fetch(
         'https://dev.to/api/articles?username=piluvitu',
         {
-          next: { revalidate: 15 },
+          next: { revalidate: 43200 },
         },
       )
       const data = await response.json()
