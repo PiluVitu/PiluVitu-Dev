058d79cc6617002c899d97313f1a0dfeee4e5472
PauloVTSilva <pilutechinformatica@gmail.com>
2024-05-12 13:01:09 -0300

    feat: add anlytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import { Analytics } from '@vercel/analytics/react'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -71,6 +73,7 @@ export default function RootLayout({
         >
           {children}
           <Toaster />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
