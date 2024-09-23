7f42dde3f22e6b4980b84a5ec1974bd715e17be5
Paulo V T Silva <pilutechinformatica@gmail.com>
2024-09-23 15:53:38 -0300

    remove redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/linkedin',
-        destination: 'https://www.linkedin.com/in/pilu-vitu/',
-        permanent: true,
-      },
-      {
-        source: '/github',
-        destination: 'https://github.com/PiluVitu',
-        permanent: true,
-      },
-      {
-        source: '/instagram',
-        destination: 'https://www.instagram.com/pilu.dev/',
-        permanent: true,
-      },
-      {
-        source: '/twitter',
-        destination: 'https://twitter.com/PiluVitu',
-        permanent: true,
-      },
-      {
-        source: '/zap',
-        destination: 'https://wa.me/message/6UOZCKTOYGUNE1',
-        permanent: true,
-      },
-      {
-        source: '/twitch',
-        destination: 'https://www.twitch.tv/piluvitu',
-        permanent: true,
-      },
-    ]
-  },
   images: {
     remotePatterns: [
       {
